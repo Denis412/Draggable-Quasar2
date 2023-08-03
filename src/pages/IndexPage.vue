5727431bbf8f35728c0c6d45050d001af07ac920
Денис Малышев <denismalysev@MacBook-Air-Denis.local>
2023-08-03 16:13:41 +0300

    init

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+    <DragTest />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import DragTest from "src/components/DragTest.vue";
+
+export default defineComponent({
+  name: "IndexPage",
+  components: {
+    DragTest,
+  },
+});
+</script>
